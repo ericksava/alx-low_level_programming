e8ca88bd656016768bb31d86965f9c98bc4bd151
Erick Sava <esava2163@gmail.com>
2022-09-09 04:37:03 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * description: 'printing to the stdout using puts function'
+ * return: Always 0 (success)
+ */
+
+int main(void)
+
+
+{
+
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+
+
+	return (0);
+}
